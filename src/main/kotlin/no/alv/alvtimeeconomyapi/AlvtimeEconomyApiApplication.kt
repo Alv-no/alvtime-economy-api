fc46979e7c1209385d5ae347fb632f5cad5436c7
Andreas Strand <andreas.strand@nav.no>
2020-01-09 18:59:20 +0100

    Initial commit

---
+++ src/main/kotlin/no/alv/alvtimeeconomyapi/AlvtimeEconomyApiApplication.kt
@@ -0,0 +1,11 @@
+package no.alv.alvtimeeconomyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlvtimeEconomyApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlvtimeEconomyApiApplication>(*args)
+}
---
+++ src/test/kotlin/no/alv/alvtimeeconomyapi/AlvtimeEconomyApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.alv.alvtimeeconomyapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlvtimeEconomyApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
